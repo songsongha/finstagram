79293b1e10ad50e3103e716cc66398955c832df0
songsongha <songsongha@gmail.com>
2021-01-19 04:51:37 +0000

    added days ago feature and updated header style

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -11,11 +11,12 @@ class FinstagramPost < ActiveRecord::Base
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
 
-        if time_ago_in_minutes >= 60
-            # the #{} evalutes the code then inserts whatever is returned into the string -->
+        if time_ago_in_minutes >= 1440  
+            "#{(time_ago_in_minutes /1440).to_i} days ago"       
+        elsif time_ago_in_minutes < 60 
+            "#{time_ago_in_minutes.to_i} minutes ago"   
+        else 
             "#{(time_ago_in_minutes / 60).to_i} hours ago"
-        else
-            "#{time_ago_in_minutes.to_i} minutes ago"
         end
     end
 
